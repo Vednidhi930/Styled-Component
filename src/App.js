d5856e2c77da843e143c8f0f86660381690b927c
Vednidhi930 <Vednidhi2312@gmail.com>
2023-12-30 18:58:39 +0530

    accordion-item

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import './App.css';
 import React, { useState } from 'react'
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Accordion from './Components.js/Accordion/Accordion'
 
-function App() {
 
-  const[count,setCount]=useState(0);
 
-  const increMent=()=>{
-    setCount(count+1);
-  }
-
-  const decreMent=()=>{
-    if(count===0){
-      setCount(0)
-      alert("Zero Limit exist!")
-    }else{
-      setCount(count-1);
-    }
-  }
-
+export default function App() {
+ 
   return (
-
     <>
-      <div className='main-div'>
-        <div className='box'>
-          <br />
-          <div className='heading'>
-            <h1>Welcome</h1>
-          </div>
-          <br />
-          <div className='count'>
-            <h1>{count}</h1>
-          </div>
-           <div className='count-btn'>
-            <Button className='btn1' onClick={increMent}>
-               <AddIcon/>
-            </Button>
-            <Button className='btn2' onClick={decreMent}>
-              <DeleteIcon/>
-            </Button>
-            </div>
-        </div>
-      </div>
+      <Accordion />
     </>
-
-  );
+  )
 }
-
-export default App;
---
+++ src/Components.js/Accordion/api.js
@@ -0,0 +1,41 @@
+export const questions=
+[
+  {
+    id:1,
+    question:" Explain Hoisting in javascript.",
+    answer:"Hoisting is the default behaviour of javascript where all the variable and function declarations are moved on top."
+
+  },
+
+  {
+    id:2,
+    question:" Explain “this” keyword.",
+    answer:"he value of the “this” keyword will always depend on the object that is invoking the function."
+  },
+
+  {
+    id:3,
+    question:" What are callbacks?",
+    answer:"A callback is a function that will be executed after another function gets executed."
+  },
+
+  {
+    id:4,
+    question:"What is syntax error in Javascript ?",
+    answer:"It occur when programmer do spelling mistakes in code."
+  },
+
+  {
+    id:5,
+    question:"What is logical Error ?",
+    answer:"It occur when logic or program is incorrect."
+  },
+
+  {
+    id:6,
+    question:" What is DOM?",
+    answer:"DOM is a programming interface for HTML and XML documents."
+  }
+]
+
+
